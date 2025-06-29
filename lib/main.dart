3bcc92790a459986fbeeac3738a1abdea54ae01f
“Urjaswee <urjaswee.chatterjee@outlook.com>
2025-06-30 02:03:18 +0530

    setup of FCM with topic subscription

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,51 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weather_app/app.dart';
 import 'package:weather_app/controllers/weather_controller.dart';
 
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   Get.put(WeatherController());
+
+//   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+//     print('🔵 Foreground message received: ${message.notification?.title}');
+//     SnackBar snackBar = SnackBar(
+//       content: Text(message.notification?.body ?? 'No message body'),
+//       duration: Duration(seconds: 3),
+//     );
+//   });
+
+//   FirebaseMessaging.instance.getInitialMessage().then((RemoteMessage? message) {
+//     if (message != null) {
+//       print(
+//           '🟢 App launched from terminated state via notification: ${message.notification?.title}');
+//       // Navigate or handle the message accordingly
+//     }
+//   });
+//   FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+//     print(
+//         '🟡 App opened from background via notification: ${message.notification?.title}');
+//     // Navigate or handle the message accordingly
+//   });
+
+//   runApp(MyApp());
+// }
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('✅ Background message received: ${message.messageId}');
+}
+
 void main() async {
-  // Initialize controllers before running app
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   Get.put(WeatherController());
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(MyApp());
 }
--- lib/services/messaging_service.dart
+++ lib/services/messaging_service.dart
@@ -1,14 +1,37 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 
 class MessagingService {
   final FirebaseMessaging _fcm = FirebaseMessaging.instance;
   Future<void> init() async {
     _fcm.requestPermission();
+    _fcm.getToken().then((token) {
+      print('🔥 FCM Token: $token');
+    });
+
+    await _fcm.subscribeToTopic("weather_test");
+
+    _fcm.requestPermission();
+
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       print("onMessage: $message");
+      SnackBar snackBar = SnackBar(
+        content: Text(message.notification?.body ?? 'No message body'),
+        duration: Duration(seconds: 3),
+      );
     });
+
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print("onMessageOpenedApp: $message");
+    });
+
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? message) {
+      if (message != null) {
+        print(
+            '🟢 App opened from terminated state: ${message.notification?.title}');
+      }
     });
   }
 }
